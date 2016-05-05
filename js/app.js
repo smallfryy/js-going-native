fa15d3da9e553475341b629bb24be99c8b7aff0e
smallfryy <holly.m.peck@gmail.com>
2016-05-05 10:56:47 -0400

    update files

--- js/app.js
+++ js/app.js
@@ -1,37 +1,47 @@
 var apiURL = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=13b1d783bf00e8dcbd3c45f47097c796&tags=tarot&per_page=24&format=json&nojsoncallback=1";
+
 var photoArray = [];
+
 var currentPhotoIndex = 0;
 
-
-// Asynch Call 
-function getHTTP(url, callBack) { 
-  var httpRequest = new XMLHttpRequest();   
+// async call 
+function getHTTP(url, callBack) {
+  
+  var httpRequest = new XMLHttpRequest();
+    
     httpRequest.onreadystatechange = function() {
       // if http is ready to go and 200 a-ok error, then load this up
       if (httpRequest.readyState === 4 && httpRequest.status === 200) {
         // collect JSON object 
         var data = JSON.parse(httpRequest.responseText);
+
         // transform scary JSON into data in thumbnail grid
           if (callBack) 
               callBack(data); 
+
           } else {error: "there was a problem"};
       };
   httpRequest.open('GET', url);
+
   httpRequest.send(); // asynch request sent to server 
 };
 
-function returnPhotoArray(scaryJSON){ 
-  photoArray = scaryJSON['photos'].photo;
+
+function returnPhotoArray(scaryJSON){
+  
+  photoArray = scaryJSON['photos'].photo; 
   return scaryJSON['photos'].photo;
 };
+ 
 
 // Main Application Logic 
-function buildPhotoViewer(data) {  
+function buildPhotoViewer(data) {
   var photos = returnPhotoArray(data); 
-  for (i = 0; i < photos.length; i++) {   
-    appendPhotoToGrid(photos[i]);   
+  for (i = 0; i < photos.length; i++) { 
+    appendPhotoToGrid(photos[i]); 
     buildPhotoLightBox(photos[i]);
   }
+
 };
 
 // User Interactions
@@ -41,48 +51,72 @@ function hideLightBox(){
 
 function showLightBox(url) {
   var photo = photoArray[currentPhotoIndex];
+  var title = photo.title;
   document.getElementById("lightbox").style.visibility = "visible";
+  document.getElementById('lightbox-image').innerHTML = '<img src="' + url + '"/>' + '<div class="title">' + title + '</div>' ;
 };
 
-function navigateRight(data) {
+
+function navigateRight(data, photo) {
   var photo = photoArray[currentPhotoIndex];
+  var imgURL = 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server + '/'+ photo.id + '_' + photo.secret + '.jpg';
   if (currentPhotoIndex < photoArray.length) {
     currentPhotoIndex += 1;
-    buildPhotoLightBox(photo);
+    document.getElementById('lightbox-image').innerHTML = '<img src="' + imgURL + '"/>' + '<div class="title">' + photo.title + '</div>';
   }
+
 }
 
+
+
 function navigateLeft(data){
-  var photo = photoArray[currentPhotoIndex];  
-  if (currentPhotoIndex > 0){ 
+  var photo = photoArray[currentPhotoIndex];
+  var title = photo.title;
+  
+  var imgURL = 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server + '/'+ photo.id + '_' + photo.secret + '.jpg';
+
+  if (currentPhotoIndex > 0){
+  
     currentPhotoIndex -= 1;
-    buildPhotoLightBox(photo);
+
+    document.getElementById('lightbox-image').innerHTML = '<img src="' + imgURL + '"/>' + '<div class="title">' + photo.title + '</div>';
+
   }
+
 };
+
 
 // DOM Manipulation
 function appendPhotoToGrid(photo){
+  // take json object and render in div element
   var thumbdiv = buildThumbnailDiv(photo);
+  // select DOM element photo-grid and throw thumbnail inside 
   document.getElementById('photo-grid').appendChild(thumbdiv);
 }; 
+
 
 // Templating Section 
 function buildThumbnailDiv(photo){
   var imgURL = 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server + '/'+ photo.id + '_' + photo.secret + '.jpg';
+  
   var inner = '<div class="title">' + photo.title + '</div>' + 
                 '<div class="image-box">' + 
                     '<img src="' + imgURL + '"/>' +
               '</div>';
+
   var iDiv = document.createElement("div");
   iDiv.setAttribute("class", "thumbnail");
   iDiv.setAttribute("onClick", "showLightBox(\'" + imgURL + "\')");
   iDiv.innerHTML = inner;
+
   return iDiv;
 }
 
 function buildPhotoLightBox(photo)  {
   var imgURL = 'https://farm' + photo.farm +'.staticflickr.com/' + photo.server + '/'+ photo.id + '_' + photo.secret + '.jpg';
   var title = photo.title;  
-   document.getElementById('lightbox-content').innerHTML = '<img src="' + imgURL + '"/>' + '<div class="title">' + title + '</div>';
+
+  // grab lightbox div 
+   document.getElementById('lightbox-image').innerHTML = '<img src="' + imgURL + '"/>' + '<div class="title">' + photo.title + '</div>';
  }
 
